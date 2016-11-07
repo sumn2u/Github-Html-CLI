50588342732f600bc80303bc9680ec59deb0b40b
suman <sumn2u@hotmail.com>
2016-11-08 01:05:45 +0545

    added github web

---
+++ index.js
@@ -0,0 +1,299 @@
+#!/usr/bin/env node
+
+"use strict";
+
+var chalk       = require('chalk');
+var clear       = require('clear');
+var CLI         = require('clui');
+var figlet      = require('figlet');
+var inquirer    = require('inquirer');
+var Preferences = require('preferences');
+var Spinner     = CLI.Spinner;
+var GitHubApi   = require('github');
+var _           = require('lodash');
+var git         = require('simple-git')();
+var touch       = require('touch');
+var fs          = require('fs');
+var path = require('path');
+var exec = require('child_process').exec;
+var files       = require('./lib/files');
+const program = require('commander'),
+pkg = require('./package.json');
+
+/**
+ * list function definition
+ *
+ */
+let hydrawebAct = (directory,options)  => {
+   console.log(chalk.red(directory+"xf"));
+    const cmd = 'ls';
+    let params = [];
+
+    if (options.all) params.push("a");
+    if (options.long) params.push("l");
+    let parameterizedCommand = params.length
+                                ? cmd + ' -' + params.join('')
+                                : cmd ;
+    if (directory) parameterizedCommand += ' ' + directory ;
+
+    let output = (error, stdout, stderr) => {
+        if (error) console.log(chalk.red.bold.underline("exec error:") + error);
+        if (stdout) console.log(chalk.green.bold.underline("Result:") + stdout);
+        if (stderr) console.log(chalk.red("Error: ") + stderr);
+    };
+
+    exec(parameterizedCommand,output);
+
+};
+
+// create html bolierplate
+var argv = require('minimist')(process.argv.slice(2))
+
+program
+    .version(pkg.version)
+    .command('init','creates a project with html, css and js boilerplate')
+    .command('list [directory]')
+    .option('-a, --all', 'List all')
+    .option('-l, --long','Long list format')
+    .action(hydrawebAct);
+
+
+program.parse(process.argv);
+
+
+// if program was called with no arguments, show help.
+if (program.args.length === 0) program.help();
+
+clear();
+console.log(
+  chalk.yellow(
+    figlet.textSync('hydra-web', { horizontalLayout: 'full' })
+  )
+);
+
+if (files.directoryExists('.git')) {
+  console.log(chalk.red('Already a git repository!'));
+  process.exit();
+}
+
+fs.writeFile('index.html', '<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1"><meta name="description" content=""><meta name="author" content=""><title>Title</title><link rel="icon" type="image/png" href="favicon.png"><link rel="stylesheet" href="assets/style.css"></head><body></body></html>', function (err) {
+  if (err) return console.log(err);
+  console.log('Hello World > helloworld.txt');
+});
+var github = new GitHubApi({
+  version: '3.0.0'
+});
+
+
+function getGithubCredentials(callback) {
+  var questions = [
+    {
+      name: 'username',
+      type: 'input',
+      message: 'Enter your Github username or e-mail address:',
+      validate: function( value ) {
+        if (value.length) {
+          return true;
+        } else {
+          return 'Please enter your username or e-mail address';
+        }
+      }
+    },
+    {
+      name: 'password',
+      type: 'password',
+      message: 'Enter your password:',
+      validate: function(value) {
+        if (value.length) {
+          return true;
+        } else {
+          return 'Please enter your password';
+        }
+      }
+    }
+  ];
+
+  inquirer.prompt(questions).then(callback);
+}
+
+function getGithubToken(callback) {
+  var prefs = new Preferences('ginit');
+
+  if (prefs.github && prefs.github.token) {
+    return callback(null, prefs.github.token);
+  }
+
+  getGithubCredentials(function(credentials) {
+    var status = new Spinner('Authenticating you, please wait...');
+    status.start();
+
+    github.authenticate(
+      _.extend(
+        {
+          type: 'basic',
+        },
+        credentials
+      )
+    );
+
+    github.authorization.create({
+      scopes: ['user', 'public_repo', 'repo', 'repo:status'],
+      note: 'ginit, the command-line tool for initalizing Git repos'
+    }, function(err, res) {
+      status.stop();
+      if ( err ) {
+        return callback( err );
+      }
+      if (res.token) {
+        prefs.github = {
+          token : res.token
+        };
+        return callback(null, res.token);
+      }
+      return callback();
+    });
+  });
+}
+
+function createRepo(callback) {
+  var argv = require('minimist')(process.argv.slice(2));
+
+  var questions = [
+    {
+      type: 'input',
+      name: 'name',
+      message: 'Enter a name for the repository:',
+      default: argv._[0] || files.getCurrentDirectoryBase(),
+      validate: function( value ) {
+        if (value.length) {
+          return true;
+        } else {
+          return 'Please enter a name for the repository';
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'description',
+      default: argv._[1] || null,
+      message: 'Optionally enter a description of the repository:'
+    },
+    {
+      type: 'list',
+      name: 'visibility',
+      message: 'Public or private:',
+      choices: [ 'public', 'private' ],
+      default: 'public'
+    }
+  ];
+
+  inquirer.prompt(questions).then(function(answers) {
+    var status = new Spinner('Creating repository...');
+    status.start();
+
+    var data = {
+      name : answers.name,
+      description : answers.description,
+      private : (answers.visibility === 'private')
+    };
+
+    github.repos.create(
+      data,
+      function(err, res) {
+        status.stop();
+        if (err) {
+          return callback(err);
+        }
+        return callback(null, res.ssh_url);
+      }
+    );
+  });
+}
+
+function createGitignore(callback) {
+  var filelist = _.without(fs.readdirSync('.'), '.git', '.gitignore');
+
+  if (filelist.length) {
+    inquirer.prompt(
+      [
+        {
+          type: 'checkbox',
+          name: 'ignore',
+          message: 'Select the files and/or folders you wish to ignore:',
+          choices: filelist,
+          default: ['node_modules', 'bower_components']
+        }
+      ]
+    ).then(function( answers ) {
+        if (answers.ignore.length) {
+          fs.writeFileSync( '.gitignore', answers.ignore.join( '\n' ) );
+        } else {
+          touch( '.gitignore' );
+        }
+        return callback();
+      }
+    );
+  } else {
+    touch('.gitignore');
+    return callback();
+  }
+}
+
+function setupRepo(url, callback) {
+  var status = new Spinner('Setting up the repository...');
+  status.start();
+  git
+    .init()
+    .add('.gitignore')
+    .add('./*')
+    .commit('Initial commit')
+    .addRemote('origin', url)
+    .push('origin', 'master')
+    .then(function(){
+      status.stop();
+      return callback();
+    });
+}
+
+function githubAuth(callback) {
+  getGithubToken(function(err, token) {
+    if (err) {
+      return callback(err);
+    }
+    github.authenticate({
+      type : 'oauth',
+      token : token
+    });
+    return callback(null, token);
+  });
+}
+
+githubAuth(function(err, authed) {
+  if (err) {
+    switch (err.code) {
+      case 401:
+        console.log(chalk.red('Couldn\'t log you in. Please try again.'));
+        break;
+      case 422:
+        console.log(chalk.red('You already have an access token.'));
+        break;
+    }
+  }
+  if (authed) {
+    console.log(chalk.green('Sucessfully authenticated!'));
+    createRepo(function(err, url){
+      if (err) {
+        console.log('An error has occured');
+      }
+      if (url) {
+        createGitignore(function() {
+          setupRepo(url, function(err) {
+            if (!err) {
+              console.log(chalk.green('All done!'));
+            }
+          });
+        });
+      }
+    });
+  }
+});
